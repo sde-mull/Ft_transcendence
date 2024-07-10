fe545d0ec29cfcd58af7e5ae1a035b43a95fdfe0
Sebastiao <sde-mull@sde-mull.com>
2024-07-10 17:38:59 +0100

    Fixed some issues with the database

--- curve/Backend/Tools/run_django_app.sh
+++ curve/Backend/Tools/run_django_app.sh
@@ -6,8 +6,8 @@ cd ../Server
 
 if [ -f "manage.py" ]; then
     # Run Django migrations
-    # python3 manage.py makemigrations
-    # python3 manage.py migrate
+    python3 manage.py makemigrations
+    python3 manage.py migrate
 
     # Start Django development server on localhost:4444
     python3 manage.py runserver 0.0.0.0:4444
